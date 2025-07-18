c76875a153eecd7168cbf9f1db2987cc7e90834e
diogo464 <diogo464@d464.sh>
2025-07-18 08:39:15 +0100

    setup network before benchmark starts

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+set -e
+cd $(dirname $0)/..
+oar-p2p net up --addresses 4/cpu --latency-matrix ./latency-matrix-5000.txt
 scripts/generate-schedule.sh | oar-p2p run --signal start:10 --output-dir benchmark-logs/ && scripts/benchmark-startup
